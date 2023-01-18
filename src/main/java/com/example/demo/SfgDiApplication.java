fa77b9b42e050b82e3f39d79d15cfdf13adcb215
Dillon Rowan <rowan.dillon@heb.com>
2023-01-18 16:14:40 -0600

    qualifier lesson

---
+++ src/main/java/com/example/demo/SfgDiApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import com.example.demo.controllers.ConstructorInjectedController;
+import com.example.demo.controllers.MyController;
+import com.example.demo.controllers.PropertyInjectedController;
+import com.example.demo.controllers.SetterInjectedController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+		String greeting = myController.sayHello();
+		System.out.println(greeting);
+
+		System.out.println("------- Property");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("------- Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("------- Constructor");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+	}
+
+}
